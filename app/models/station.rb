e577509d5dea57cf313fc73abf878cec893ee9d4
Anna Royer <anna.royer@colorado.edu>
2018-02-18 13:52:44 -0700

    Add slug

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,6 +1,6 @@
 class Station < ApplicationRecord
   extend FriendlyId
-  friendly_id :name, use: :slugged
+  friendly_id :name, :use => :slugged
   validates_presence_of   :name,
                           :dock_count,
                           :city,
