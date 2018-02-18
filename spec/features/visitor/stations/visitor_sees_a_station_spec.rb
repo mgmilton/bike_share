5a2f0adbca7a779b56fc63ce6ee40af4a78b7f61
Anna Royer <anna.royer@colorado.edu>
2018-02-18 13:33:13 -0700

    Add visitor sees all station attributes test

--- spec/features/visitor/stations/visitor_sees_a_station_spec.rb
+++ spec/features/visitor/stations/visitor_sees_a_station_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 context 'As a visitor' do
   describe 'when I visit the station show' do
+    before(:each) { @station = create(:station) }
     scenario "I see the url '/:station-name' with that station's name instead of :id" do
-      station = create(:station)
-
-      visit station_path(station)
+      visit station_path(@station)
 
       expect(current_url).to eq('/stations/Station-1')
     end
     scenario 'I see all attributes for that station.' do
+      visit station_path(@station)
 
+      expect(page).to have_content(@station.name)
+      expect(page).to have_content(@station.dock_count)
+      expect(page).to have_content(@station.city)
+      expect(page).to have_content(@station.format_date)
+      expect(page).to have_content(@station.latitude)
+      expect(page).to have_content(@station.longitude)
     end
   end
 end
