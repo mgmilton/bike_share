e3bf80bb920e45501eccf7f779ab3197cd038351
Anna Royer <anna.royer@colorado.edu>
2018-02-18 12:53:46 -0700

    Add test for format date instance method on station model

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Station, type: :model do
-  describe 'validations' do
+  context 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :dock_count }
     it { should validate_presence_of :city }
@@ -10,7 +10,18 @@ describe Station, type: :model do
     it { should validate_presence_of :longitude }
   end
 
-  describe 'uniqueness' do
+  context 'uniqueness' do
     it { should validate_uniqueness_of :name }
+  end
+
+  context 'instance methods' do
+    describe '#format_date' do
+      it 'formats the installation date' do
+        date = Date.new(2013,8,20)
+        station = create(:station, installation_date: date)
+
+        expect(station.format_date).to eq('August 20, 2013')
+      end
+    end
   end
 end
