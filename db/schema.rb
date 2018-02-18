ba4fd4804bd1c401a090bde283ee60bbcc6d2ee8
mgmilton <matt.milton@gmail.com>
2018-02-18 13:35:06 -0700

    adds authentication navigation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218182251) do
+ActiveRecord::Schema.define(version: 20180218202852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20180218182251) do
     t.decimal "mean_wind_speed"
     t.decimal "mean_precipitation"
     t.integer "zip_code"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
