c45a16691dc7f448553bd205872622253a35f22f
Anna Royer <anna.royer@colorado.edu>
2018-02-18 11:28:08 -0700

    Add test for visitor sees stations index

---
+++ spec/features/visitor/stations/visitor_sees_all_stations_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+context 'As a visitor' do
+  describe 'When I visit the stations index,' do
+    scenario 'I see all stations (name, dock count, city, installation date)' do
+      stations = create_list(:stations)
+
+      visit stations_path
+
+      stations.each do |station|
+        expect(page).to have_content(station.name)
+        expect(page).to have_content(station.dock_count)
+        expect(page).to have_content(station.city)
+        expect(page).to have_content(station.installation_date)
+      end
+    end
+  end
+end
