217802ea85a323f5a7e2181c90966820f6fcc5ff
mgmilton <matt.milton@gmail.com>
2018-02-18 14:38:10 -0700

    adds dashboard route for users#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :conditions, only: [:index, :show]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create]
 
+  get "/dashboard", to: "users#show"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
