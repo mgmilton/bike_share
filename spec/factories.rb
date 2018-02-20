20ccb8c8ac7dc2bdc6de958c29f4dbe928543f75
Anna Royer <anna.royer@colorado.edu>
2018-02-20 12:08:46 -0700

    Update factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,16 +1,5 @@
 FactoryBot.define do
   factory :condition do
-<<<<<<< HEAD
-    sequence(:date) { |n| "n/n/n" }
-    sequence(:max_temperature) {|n| "Max Temp #{n}"}
-    sequence(:mean_temperature) {|n| "Mean Temp #{n}"}
-    sequence(:min_temperature) {|n| "Min Temp #{n}"}
-    sequence(:mean_humidity) {|n| "Mean Humidity #{n}"}
-    sequence(:mean_visibility) {|n| "Mean Visibility #{n}"}
-    sequence(:mean_wind_speed) {|n| "Mean Wind Speed #{n}"}
-    sequence(:mean_precipitation) {|n| "Mean Precipitation#{n}"}
-    sequence(:zip_code) {|n| "Zip Code #{n}"}
-=======
     date Time.now
     max_temperature 105.0
     min_temperature 30.0
@@ -20,7 +9,6 @@ FactoryBot.define do
     mean_visibility 3.0
     mean_precipitation 0.0
     zip_code  "91407"
->>>>>>> adds model tests
   end
 
   factory :user do
