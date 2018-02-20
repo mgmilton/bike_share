c93b0d1a081869b712d4170f4789feaeaae5087d
mgmilton <matt.milton@gmail.com>
2018-02-20 14:23:51 -0700

    updates seed file

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,6 +1,5 @@
 class Condition < ApplicationRecord
   has_many :trips
-<<<<<<< HEAD
   before_validation :check_zip_code
 
   def check_zip_code
@@ -8,32 +7,8 @@ class Condition < ApplicationRecord
       self.zip_code = zip_codes["San Francisco"]
     end
   end
-=======
-  # validates_presence_of :date, :max_temperature, :mean_temperature, :min_temperature, :mean_humidity, :zip_code
->>>>>>> adds databse relations
 
-  def self.max_temperature
-    maximum(:max_temperature)
-  end
-
-  def self.min_temperature
-    minimum(:max_temperature)
-  end
-
-  def self.max_precipitation
-    maximum(:mean_precipitation)
-  end
-
-  def self.min_precipitation
-    minimum(:mean_precipitation)
-  end
-
-  def self.min_wind_speed
-    minimum(:mean_wind_speed)
-  end
-
-<<<<<<< HEAD
-  def self.set_divisions(floor,ceiling, increment)
+  def self.set_divisions(floor, ceiling, increment)
     ((ceiling-floor)/increment).ceil
   end
 
@@ -149,28 +124,4 @@ class Condition < ApplicationRecord
       result
     end
   end
-=======
-  def self.max_wind_speed
-    maximum(:mean_wind_speed)
-  end
-
-  def self.max_visibility
-    maximum(:mean_visibility)
-  end
-
-  def self.min_visibility
-    minimum(:mean_visibility)
-  end
-
-  def self.set_divisions(floor,ceiling, increment)
-    ((ceiling-floor)/increment).ceil
-  end
-
-  def self.set_range(floor, ceiling, increment)
-    (0..set_divisions(floor, ceiling, increment)).map do |num|
-      [(floor + num * increment).round(1), (floor + (num+1) * increment).round(1)]
-    end
-  end
-
->>>>>>> adds databse relations
 end
