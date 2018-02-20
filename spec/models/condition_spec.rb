885630089625dfcbfec58cff74f6a89ff6a03959
Anna Royer <anna.royer@colorado.edu>
2018-02-20 12:53:00 -0700

    Add missing end

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -1,14 +1,6 @@
 require "rails_helper"
 
 describe Condition, type: :model do
-  describe "validations" do
-    # it {should validate_presence_of(:date)}
-    # it {should validate_presence_of(:max_temperature)}
-    # it {should validate_presence_of(:mean_temperature)}
-    # it {should validate_presence_of(:min_temperature)}
-    # it {should validate_presence_of(:mean_humidity)}
-    # it {should validate_presence_of(:zip_code)}
-  end
 
   describe "class methods" do
     before(:each) do
