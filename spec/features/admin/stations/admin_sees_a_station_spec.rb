be0f5df6fdfe4398b42916e394a6de21a97c239a
Anna Royer <anna.royer@colorado.edu>
2018-02-20 17:25:00 -0700

    Add edit delete station buttons to index and show

---
+++ spec/features/admin/stations/admin_sees_a_station_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+context 'As an admin' do
+  describe 'When I visit a station show, in addition to everything a visitor can see' do
+    before(:each) do
+      admin = create(:admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      @station = create(:station)
+      create(:trip, station: @station, start_station_id: @station.id, end_station_id: @station.id)
+    end
+    scenario 'I see a button next to each station to edit that station' do
+      visit station_path(@station)
+
+      expect(page).to have_link('Edit')
+    end
+
+    scenario 'I also see a button next to each station to delete that station' do
+      visit station_path(@station)
+
+      expect(page).to have_link('Delete')
+    end
+  end
+end
