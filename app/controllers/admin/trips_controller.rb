5d796cfd8f2b632b591689a6e1381b9181ce94d9
slimecog <wlcjohnson@gmail.com>
2018-02-21 21:50:11 -0700

    adds admin trip create functionality, test won't pass, huge surprise.

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TripsController < Admin::BaseController
-  before_action :require_admin, only: [:new, :create, :edit, :destroy]
+  before_action :set_trip, only: [:edit, :update, :destroy]
 
   def new
     @trip = Trip.new
@@ -7,6 +7,7 @@ class Admin::TripsController < Admin::BaseController
 
   def create
     @trip = Trip.new(trip_params)
+    @trip.station_id = @trip.start_station_id
     if @trip.save
       flash[:success] = "Trip Successfully Saved"
       redirect_to trip_path(@trip)
@@ -21,7 +22,6 @@ class Admin::TripsController < Admin::BaseController
   end
 
   def update
-    @trip = Trip.find(params[:id])
     if @trip.update(trip_params)
       flash[:success] = "Trip Successfully Updated"
       redirect_to trip_path(@trip)
@@ -32,7 +32,6 @@ class Admin::TripsController < Admin::BaseController
   end
 
   def destroy
-    @trip = Trip.find(params[:id])
     if @trip.destroy
       flash[:success] = "Trip Successfully Deleted"
       redirect_to trips_path
@@ -47,5 +46,9 @@ class Admin::TripsController < Admin::BaseController
 
     def trip_params
       params.require(:trip).permit(:duration, :start_date, :start_station_id, :end_date, :end_station_id, :bike_id, :subscription_type, :zip_code)
+    end
+
+    def set_trip
+      @trip = Trip.find(params[:id])
     end
 end
