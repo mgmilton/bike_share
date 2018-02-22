470c2b36a63718c54091108d5a1db91df61fb17f
Anna Royer <anna.royer@colorado.edu>
2018-02-21 17:59:40 -0700

    rename test

--- spec/features/user/conditions/user_can_see_rides_by_weather_spec.rb
+++ spec/features/user/conditions/user_can_see_rides_by_weather_spec.rb
@@ -5,7 +5,7 @@ describe "As a registered user and admin" do
     it "displays the breakdown of rides by weather conditions" do
       create_list(:condition, 10)
       visit "/weather-dashboard"
-  
+
       expect(page).to have_content("Amount of Bike Rides when High Temperature was between")
       expect(page).to have_content("Amount of Bike Rides when Precipitation was between")
       expect(page).to have_content("Amount of Bike Rides when Wind Speed was between")
