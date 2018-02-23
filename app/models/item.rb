5370d4b06df6a0131220fb9aeb6d2e02162b9cf0
Anna Royer <anna.royer@colorado.edu>
2018-02-23 09:00:34 -0700

    Add paperclip image and enum to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
-
+  enum status: %w(active, retired)
+  has_attached_file :image,
+                    styles: { thumb: ["64x64#", :jpg] }
+  validates_attachment :image,
+                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
