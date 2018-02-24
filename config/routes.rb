9133fac7ed02666b56726e683c947f72e75571b3
Anna Royer <anna.royer@colorado.edu>
2018-02-24 10:33:36 -0700

    reorganize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,18 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
-  resources :users, only: [:new, :create]
-  get "/dashboard", to: "users#show"
-
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
-
-  resources :users, only: [:new, :create]
-
-  resources :trips, only: [:index, :show]
-
-  resources :stations, only: [:index, :show], param: :name
-
-  resources :carts, only: [:create]
-
   namespace :admin do
-  	resources :conditions, only: [:new, :create, :edit, :update, :destroy]
+    resources :conditions, only: [:new, :create, :edit, :update, :destroy]
     resources :stations, except: [:index, :show], param: :name
     resources :trips, only: [:new, :edit, :update, :destroy]
     resources :items, only: [:create]
     get 'bike-shop/new', to: 'items#new'
   end
+
+  resources :carts, only: [:create]
+  resources :conditions, only: [:index, :show]
+  resources :trips, only: [:index, :show]
+  resources :stations, only: [:index]
 
   get "/dashboard", to: "users#show"
   get "/login", to: "sessions#new"
@@ -36,10 +26,5 @@ Rails.application.routes.draw do
 
   get 'bike-shop', to: 'items#index'
 
-  resources :conditions, only: [:index, :show]
-  resources :trips, only: [:index, :show]
-  resources :stations, only: [:index]
   get '/:name', to: 'stations#show'
-
-  get 'bike-shop/new', to: 'items#new'
 end
