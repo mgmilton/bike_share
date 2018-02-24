f1cdf741b9fe1473f23881024dcc536ac4ad1908
Anna Royer <anna.royer@colorado.edu>
2018-02-24 13:37:25 -0700

    add slug to items table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224202536) do
+ActiveRecord::Schema.define(version: 20180224203101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180224202536) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.string "slug"
   end
 
   create_table "order_items", force: :cascade do |t|
