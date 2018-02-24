81ccc52393f191f9e62cc833b2036e162a43b3c9
mgmilton <matt.milton@gmail.com>
2018-02-24 11:31:39 -0700

    resolves the merge conflicts

--- spec/features/visitor/logins/visitor_can_login_as_a_user_spec.rb
+++ spec/features/visitor/logins/visitor_can_login_as_a_user_spec.rb
@@ -17,6 +17,10 @@ describe "As a visitor" do
       expect(page).to have_content("Email: #{user.email}")
       expect(page).to have_content("Account Created: #{user.created_at}")
       expect(page).to_not have_content("Login")
+<<<<<<< HEAD:spec/features/visitor/logins/visitor_can_login_as_a_user_spec.rb
+=======
+      expect(page).to have_content("Log Out")
+>>>>>>> resolves the merge conflicts:spec/features/visitor/logins/visitor_can_login_as_a_user_spec.rb
     end
   end
 end
