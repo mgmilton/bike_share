cafaaafdd93af50b20600ab74df9e20824246957
Anna Royer <anna.royer@colorado.edu>
2018-02-24 12:19:17 -0700

    add memoization and before action for set_cart method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :current_admin?
   before_action :set_cart
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -15,6 +16,6 @@ class ApplicationController < ActionController::Base
   end
 
   def set_cart
-    @cart = Cart.new(session[:cart])
+    @cart ||= Cart.new(session[:cart])
   end
 end
