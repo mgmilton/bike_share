6f8f8f2c9a6fa17e348386776294ef6af5731d33
slimecog <wlcjohnson@gmail.com>
2018-02-26 11:18:52 -0700

    adds assets to production

--- spec/features/visitor/logins/visitor_can_login_as_a_user_spec.rb
+++ spec/features/visitor/logins/visitor_can_login_as_a_user_spec.rb
@@ -10,17 +10,13 @@ describe "As a visitor" do
       fill_in "email", with: user.email
       fill_in "password", with: user.password
 
-
       click_on "Log In"
 
       expect(page).to have_content("Logged in as #{user.name}")
       expect(page).to have_content("Email: #{user.email}")
       expect(page).to have_content("Account Created: #{user.created_at}")
       expect(page).to_not have_content("Login")
-<<<<<<< HEAD:spec/features/visitor/logins/visitor_can_login_as_a_user_spec.rb
-=======
       expect(page).to have_content("Log Out")
->>>>>>> resolves the merge conflicts:spec/features/visitor/logins/visitor_can_login_as_a_user_spec.rb
     end
   end
 end
