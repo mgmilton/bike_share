c7ca718caee6765628205e2d3fdb69024639481f
Anna Royer <anna.royer@colorado.edu>
2018-02-26 11:31:43 -0700

    add styling

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -10,4 +10,9 @@ class StationsController < ApplicationController
   def dashboard
     @stations = Station.all
   end
+
+  private
+    def require_user
+      render file: "public/404" unless session[:current_user] == current_user
+    end
 end
