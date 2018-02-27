a6b869f982d6f8dc7f68aa04fd3b4e080c3047a7
Anna Royer <anna.royer@colorado.edu>
2018-02-26 17:56:23 -0700

    add address and full name columns to user

---
+++ db/migrate/20180227004617_add_columns_to_user.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :text
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zip_code, :string
+  end
+end
